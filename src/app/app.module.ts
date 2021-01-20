0212278f8dc52a3dd532942178c26608322d75f0
muminovicanel <muminovic____@hotmail.ch>
2021-01-20 16:57:35 +0100

    add modules from Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 
 const materialModules = [
   MatIconModule,
   MatToolbarModule,
-  MatButtonModule,
   MatTabsModule,
   MatInputModule
 ];
