62dc0c2031f406c52b8d3c4c703e5e24241104c8
muminovicanel <muminovic____@hotmail.ch>
2021-01-20 17:03:25 +0100

    add functions that validates input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+import {
+  pipeAll
+} from './utils/pipe.function';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-  changeText: string = "write something in the input field to change this label";
-  inputText: string = "";
+  public displayErrors: string[] = [];
+  public changeText = 'write something in the input field to change this label';
+  public inputText = '';
+  public isNumberInStringText = false;
+  public isLongEnoughText = false;
+  public isIntegerText = false;
 
 
-  setLabelText(value: string) {
-    console.log(value)
-    if (value === "") {
-      this.changeText = "write something in the input field to change this label";
+  public setLabelText = (value: string): void => {
+    if (value === '') {
+      this.changeText = 'write something in the input field to change this label';
     } else {
-      this.changeText = value;
+      const newValueValidator = (data: string) => pipeAll(
+        this.isNumberInString,
+        this.checkLength,
+        this.isInteger
+      )(value);
+      console.log(newValueValidator(value));
+      if (newValueValidator(value)) {
+        this.changeText = value;
+      } else {
+        this.changeText = 'write something in the input field to change this label';
+      }
     }
   }
+
+  public validateInput = (value: string): string => {
+    if (!this.checkLength(value) && !this.isNumberInString(value)) {
+      return 'the inserted text is too short and it does not contain a 9';
+    }
+    if (!this.isInteger(value)) {
+      return 'the inserted text is not an integer';
+    }
+    return 'validated';
+  }
+
+  private isInteger = (value: string): boolean => {
+    this.isIntegerText = Number.isInteger(+value);
+    return Number.isInteger(+value);
+  }
+
+  private checkLength = (value: string): boolean => {
+    if (value && value.length < 3) {
+      this.isLongEnoughText = false;
+      return false;
+    }
+    console.log(this.isLongEnoughText);
+    this.isLongEnoughText = true;
+    return true;
+  }
+
+  private isNumberInString = (value: string): boolean => {
+    this.isNumberInStringText = value.includes('9');
+    return value.includes('9');
+  }
+
 }
